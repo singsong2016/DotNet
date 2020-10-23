99c59df7291caae1f3b6bdac545e9e680a4c8aeb
jdc <229931879@qq.com>
2020-10-23 18:08:29 +0800

    Update tcpClient.cs
    Tcp  Server and Client  Mirror

--- snippets/tcpClient.cs
+++ snippets/tcpClient.cs
@@ -1,8 +1,67 @@
-var tclient = new TcpClient("127.0.0.1", 1028);
+  static void Main(string[] args)
+        {
+            Task.Factory.StartNew(TcpServer);
 
-tclient.Client.Send(Encoding.UTF8.GetBytes("what are you doing here"));
+            client();
+            
+        }
 
-var b = new Byte[1024];
-var length = tclient.Client.Receive(b);
-var result = Encoding.UTF8.GetString(b, 0, length);
-MessageBox.Show(result);
+        static void TcpServer()
+        {
+            var a = new TcpListener(IPAddress.Parse("127.0.0.1"), 1028);
+
+            a.Start();
+            Console.WriteLine("started");
+
+            while (true)
+            {
+                var c = a.AcceptTcpClient();
+
+                while (c.Connected)
+                {
+
+                    try
+                    {
+                        var buffer = new byte[c.ReceiveBufferSize];
+
+                        var len = c.Client.Receive(buffer);
+
+                        var s = Encoding.UTF8.GetString(buffer, 0, len);
+
+                        Console.WriteLine(s);
+
+                        Console.WriteLine(c.Client.RemoteEndPoint);
+
+                        c.Client.Send(Encoding.UTF8.GetBytes(s.Length.ToString()));
+                        
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("disconnected");
+                    }
+
+                }
+
+            }
+        }
+
+        static void client()
+        {
+            var a = new TcpClient("127.0.0.1", 1028);
+
+            while (a.Connected)
+            {
+                var s = Console.ReadLine();
+
+                a.Client.Send(Encoding.UTF8.GetBytes(s));
+
+                var buffer = new byte[a.ReceiveBufferSize];
+
+                var len = a.Client.Receive(buffer);
+
+                var result = Encoding.UTF8.GetString(buffer, 0, len);
+
+                Console.WriteLine(result);
+
+            }
+        }
