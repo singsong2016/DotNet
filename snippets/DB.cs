32ff1edc08e3964e5bb446ba2cce9fc6c83095e0
jdc <229931879@qq.com>
2020-10-27 14:27:34 +0800

    Update DB.cs

--- snippets/DB.cs
+++ snippets/DB.cs
@@ -9,24 +9,23 @@ namespace console
     public class DB
     {
 
-        private static string constr = "data source=localhost;user id=root;password=123456;database=mysql";
-     
-        public static DataTable getData(string sql, out string msg)
+        private static readonly string db = "data source=data.db";
+
+        private static SQLiteConnection con=new SQLiteConnection(db);
+
+        public static DataTable GetData(string sql)
         {
-            var con = new MySqlConnection(constr);
             try
             {
                 con.Open();
                 var dt = new DataTable();
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, con);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(sql, con);
                 da.Fill(dt);
                 dt.TableName = "return data table";
-                msg = "";
                 return dt;
             }
             catch (Exception e)
             {
-                msg = e.Message;
                 return null;
             }
             finally
@@ -35,20 +34,18 @@ namespace console
             }
         }
 
-        public static int executeSql(string sql, out string msg)
+        public static int ExecuteSql(string sql)
         {
-            var con = new MySqlConnection(constr);
+          
             try
             {
                 con.Open();
-                MySqlCommand cmd = new MySqlCommand(sql, con);
+                var cmd = new SQLiteCommand(sql, con);
                 int n = cmd.ExecuteNonQuery();
-                msg = "";
                 return n;
             }
             catch (Exception e)
             {
-                msg = e.Message;
                 return 0;
             }
             finally
