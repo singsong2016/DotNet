8006b8bd4fb30bc5b97ebe99151ef62fdf2ce63c
jdc <jdcworldmail@gmail.com>
2021-05-07 10:52:51 +0800

    Update ArrayDemo.cs

--- SharedCs/ArrayDemo.cs
+++ SharedCs/ArrayDemo.cs
@@ -22,3 +22,17 @@ public static void ArraySkipCopyDemo()
     }
 
 }
+
+------------------------------查找array元素-------------
+    private static void ArrayIndex()
+    {
+        var a = new (string mid, string name)[2];
+
+        a[0] = ("13", "wang");
+
+        a[1] = ("123", "liu");
+
+        var i = Array.FindIndex(a, x => x.mid == "123");
+
+        Console.WriteLine(i);
+    }    
