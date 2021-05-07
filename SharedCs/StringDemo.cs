4aa0acc34abba339accc8d30e18e7e2e3c1bbd18
jdc <jdcworldmail@gmail.com>
2021-05-07 10:36:10 +0800

    Update StringDemo.cs

--- SharedCs/StringDemo.cs
+++ SharedCs/StringDemo.cs
@@ -5,3 +5,16 @@ var a = new int[3] { 2, 3, 4 };
 var b = string.Join(",", a);
 Console.WriteLine(b);
 #endregion
+
+
+ public static void ToStringDemo()
+{
+    var a = 100;
+
+    Console.WriteLine(a.ToString("P") + " P");
+    Console.WriteLine(a.ToString("C") + " C");
+    Console.WriteLine(a.ToString("F1") + " F1");
+    Console.WriteLine(a.ToString("x2") + " x2");
+    Console.WriteLine(a.ToString("N1") + " N1");
+    Console.WriteLine(a.ToString("0000") + " 0000");
+}
