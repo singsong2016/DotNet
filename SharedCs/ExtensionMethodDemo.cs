ad51471cb082cd0a56f297e1b40fffef2b1b9742
jdc <jdcworldmail@gmail.com>
2021-05-07 10:07:56 +0800

    Update ExtensionMethodDemo.cs

--- SharedCs/ExtensionMethodDemo.cs
+++ SharedCs/ExtensionMethodDemo.cs
@@ -1,3 +1,10 @@
+
+   public static void ExtensionDemo()
+        {
+            var a = 100;
+            a.Hi("params for Hi").go();  //block to execute
+        }
+
  static class ExtensionMethod
     {
         public static string Hi(this int a, string s)
