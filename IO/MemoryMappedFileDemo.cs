e7b65eba33a559857b2f2d60d572fb84c15eb4f9
jdc <jdcworldmail@gmail.com>
2021-05-11 09:20:32 +0800

    Update and rename SharedCs/MemoryMappedFileDemo.cs to IO/MemoryMappedFileDemo.cs

---
+++ IO/MemoryMappedFileDemo.cs
@@ -0,0 +1,29 @@
+#region 文件映射到内存，可以供一台电脑大量独立程序共同读写访问交互
+public static void MemoryMappedFileDemo()
+{
+    Create();
+    Read();
+
+    static void Create()
+    {
+        var a = MemoryMappedFile.CreateNew("shared memory", 1);
+
+        using var b = a.CreateViewAccessor();
+        b.Write(10, 20000000);
+    }
+
+    static void Read()
+    {
+
+#pragma warning disable CA1416 // Validate platform compatibility
+        var a = MemoryMappedFile.OpenExisting("shared memory");
+#pragma warning restore CA1416 // Validate platform compatibility
+
+        using var b = a.CreateViewAccessor();
+        short d = 0;
+        b.Read(10, out d);
+        Console.WriteLine(d);
+    }
+}
+
+#endregion
