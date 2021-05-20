66b9d87e99b7cb77561479efd2df076c715ccfd1
jdc <jdcworldmail@gmail.com>
2021-05-20 15:34:16 +0800

    Update ShaEncrypt.cs

--- SharedCs/ShaEncrypt.cs
+++ SharedCs/ShaEncrypt.cs
@@ -34,6 +34,4 @@ public string Sha256HashFromString(string strData)
        throw new Exception(ex.Message);
     }
 }
-————————————————
-版权声明：本文为CSDN博主「杰丶Sue」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_16256429/article/details/78258782
+
