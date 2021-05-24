f3547bc677f0cbeb8429dada7807bdf8d4ceba0d
jdc <jdcworldmail@gmail.com>
2021-05-24 17:24:30 +0800

    Update DateTimes.cs

--- SharedCs/DateTimes.cs
+++ SharedCs/DateTimes.cs
@@ -5,3 +5,11 @@ var format = "ddMMMyy";
 var a = DateTime.ParseExact("08MAY21", format, CultureInfo.InvariantCulture);
 
 Console.WriteLine(a);
+
+
+-------------------------try parse  exact-----------------
+  CultureInfo enUS = new CultureInfo("en-US");
+  DateTime d;
+  var a = DateTime.TryParseExact("202105", "yyyyMMdd", enUS, DateTimeStyles.None, out d);
+
+  Console.WriteLine(d.Year);//true:2021  false:1
